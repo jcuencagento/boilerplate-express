46daf6893038f3115ec089e2e29e96f91d5cd586
Javier Cuenca Gento <80844803+jcuencagento@users.noreply.github.com>
2022-11-03 19:03:01 +0100

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -3,9 +3,11 @@ let app = express();
 
 console.log("Hello World")
 
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html");
+  })
 
-
-
+  app.use("/public", express.static(__dirname +"/public"))
 
 
 
