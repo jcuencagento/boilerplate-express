87b5414f80ec7d2dc074d4ccc360d8bdddc42ad9
Javier Cuenca Gento <80844803+jcuencagento@users.noreply.github.com>
2022-11-03 22:55:52 +0100

    Commit

--- myApp.js
+++ myApp.js
@@ -3,12 +3,30 @@ let app = express();
 
 console.log("Hello World")
 
+app.use((req, res, next) => {
+  console.log(req.method+" "+req.path+" - "+req.ip);
+  next();
+});
+
+app.use("/public", express.static(__dirname +"/public"));
+
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
-  })
+  });
 
-  app.use("/public", express.static(__dirname +"/public"))
+app.get("/json", (req, res) => {
+    response = "Hello json"
+    if (process.env.MESSAGE_STYLE === "uppercase") response = response.toUpperCase(); 
+    res.json({"message": response});
+  });
 
+app.get('/now', function(req, res, next) {
+    req.time = new Date().toString();  // Hypothetical synchronous operation
+    next();
+  }, function(req, res) {
+    res.json({"time": req.time});
+  });
 
 
 
